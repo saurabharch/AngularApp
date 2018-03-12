7b831f7fbd4597a7e3b56f5eb3dc126d42b1101b
saurabharch <saurabharch@users.noreply.github.com>
2018-03-12 23:39:58 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCM6CZKFC2vaScLo1EymV40mW3VEacqlbo',
-    authDomain: 'angularapp-9c30d.firebaseapp.com',
-    databaseURL: 'https://angularapp-9c30d.firebaseio.com',
-    projectId: 'angularapp-9c30d',
-    storageBucket: 'angularapp-9c30d.appspot.com',
-    messagingSenderId: '1063065916140'
+     apiKey: '<API Key>',
+    authDomain: '<Authorize Domain Name>',
+    databaseURL: '<Database Url>',
+    projectId: '<Project ID>',
+    storageBucket: '<Storage bucket address>',
+    messagingSenderId: '<Message Sender ID>'
   }
 };
